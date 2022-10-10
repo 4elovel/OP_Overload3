e5468b01e43d9d78c168663cfd8b8c8bea1955b8
4elovel <marcynakvova007@gmail.com>
2022-10-10 20:45:51 +0300

    Добавьте файлы проекта.

---
+++ OP_Overload3/OP_Overload3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Str {
+private:
+    string st; 
+public:
+    Str()
+    {
+        st = "";
+    }
+
+    Str(string str)
+    {
+        st = str;
+    }
+
+    void Print() {
+        cout << st << "\n";
+    }
+
+    Str operator*(const Str& N)
+    {
+        Str temp;
+        for (size_t i = 0; i < st.size(); i++)
+        {
+            for (size_t j = 0; j < N.st.size(); j++)
+            {
+                if (st[i]==N.st[j])
+                {
+                    temp.st += st[i];
+                }
+            }
+        }
+        return temp;
+    }
+};
+int main()
+{
+    Str a("HasefGljklosghj!");
+    Str b("zxcvvnHnm@^V!zCelo");
+    Str c = a * b;
+    c.Print();
+}
